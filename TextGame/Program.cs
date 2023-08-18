0ef48c789d894b2bb46d7ab8db00390f128c0a85
KHS <kij4300@naver.com>
2023-08-18 19:46:56 +0900

    게임 시작 화면

--- TextGame/Program.cs
+++ TextGame/Program.cs
@@ -4,30 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("마을에 오신 여러분 환영합니다.");
-            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
-            Console.WriteLine();
-            Console.WriteLine("1. 상태 보기");
-            Console.WriteLine("2. 인벤토리");
-            Console.WriteLine();
-            Console.WriteLine("원하시는 행동을 입력해주세요.");
-            Console.Write(">> ");
+            bool gameOver = false;
 
-            int input = int.Parse(Console.ReadLine());
-            
-            if (input == 1)
+            int input;
+
+            while (!gameOver)
             {
-                Console.WriteLine(input);
+                
+
+                StartMenu();
             }
-            else if (input == 2)
+
+
+
+            void StartMenu()
             {
-                Console.WriteLine(input);
-            }
-            else
-            {
-                Console.WriteLine("잘못된 입력입니다.");
+                Console.Clear();
+                Console.WriteLine("마을에 오신 여러분 환영합니다.");
+                Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+                Console.WriteLine();
+                Console.WriteLine("1. 상태 보기");
+                Console.WriteLine("2. 인벤토리");
+                Console.WriteLine();
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+
+                input = int.Parse(Console.ReadLine());
             }
 
         }
     }
 }
+
