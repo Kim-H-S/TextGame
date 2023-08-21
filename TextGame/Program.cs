94fd66d7d14327d5252c0774f5d28ac4a45f721c
KHS <kij4300@naver.com>
2023-08-21 11:13:13 +0900

    아이템 클래스

--- TextGame/Program.cs
+++ TextGame/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection.Emit;
+﻿using System;
+using System.Numerics;
+using System.Reflection.Emit;
 using System.Xml.Linq;
 
 namespace TextGame
@@ -22,6 +24,13 @@ namespace TextGame
             player = new Player(1, "Chad", "전사", 10, 5, 100, 1500);
 
             // 아이템 정보 셋팅
+            Item IronArmor = new Item("무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 9, 0, 0, true, true);
+            Item WornSword = new Item("낡은 검", "쉽게 볼 수 있는 낡은 검입니다.", 2, 0, 0, 0, true, true);
+
+            Item TraineeArmor = new Item("수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", 0, 5, 0, 1000, false, false);
+            Item SpartanArmor = new Item("스파르타의 갑옷", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 0, 15, 0, 3500, false, false);
+            Item BronzeAxe = new Item("청동 도끼", "어디선가 사용됐던거 같은 도끼입니다.", 5, 0, 0, 1500, false, false);
+            Item SpartanSpear = new Item("스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", 7, 0, 0, 10, false, false);
         }
 
         static void DisplayGameIntro()
@@ -72,7 +81,7 @@ namespace TextGame
             Console.Write(">> ");
 
             int input = CheckValidInput(0, 0);
-            switch(input) 
+            switch (input)
             {
                 case 0:
                     DisplayGameIntro();
@@ -104,15 +113,15 @@ namespace TextGame
     }
 
 
-    public class Player
+    class Player
     {
-        public int level { get; }
-        public string name { get; }
-        public string classType { get; }
-        public float attack { get; }
-        public float defense { get; }
-        public float health { get; }
-        public long gold { get; }
+        public int level { get; set; }
+        public string name { get; set; }
+        public string classType { get; set; }
+        public float attack { get; set; }
+        public float defense { get; set; }
+        public float health { get; set; }
+        public long gold { get; set; }
 
         public Player(int level, string name, string classType, float attack, float defense, float health, long gold)
         {
@@ -126,8 +135,37 @@ namespace TextGame
         }
 
         
+
+
     }
 
+    public class Item
+    {
+        public string name;
+        public string description;
 
+        public int attack;
+        public int defense;
+        public int health;
+
+        public long gold;
+
+        // 구입 유무
+        public bool isBuy;
+        // 장착 유무
+        public bool isEquip;
+
+        public Item(string name, string description, int attack, int defense, int health, long gold, bool isBuy, bool isEquip)
+        {
+            this.name = name;
+            this.description = description;
+            this.attack = attack;
+            this.defense = defense;
+            this.health = health;
+            this.gold = gold;
+            this.isBuy = isBuy;
+            this.isEquip = isEquip;
+        }
+    }
 }
 
